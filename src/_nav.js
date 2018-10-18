5105f4ab7982400fcc344e3dc44fec5ae4bd58cc
Nguyễn Văn Tiệp <anhtiephyv@gmail.com>
2018-10-18 17:46:59 +0700

    cccccc

--- src/_nav.js
+++ src/_nav.js
@@ -20,11 +20,11 @@ export default {
     {
       title: true,
       name: 'Theme',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      wrapper: {            
+        element: '',       
+        attributes: {}      
       },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
+      class: ''            
     },
     {
       name: 'Colors',
@@ -263,18 +263,6 @@ export default {
         },
       ],
     },
-    // {
-    //   name: 'Download CoreUI',
-    //   url: 'http://coreui.io/react/',
-    //   icon: 'icon-cloud-download',
-    //   class: 'mt-auto',
-    //   variant: 'success',
-    // },
-    // {
-    //   name: 'Try CoreUI PRO',
-    //   url: 'http://coreui.io/pro/react/',
-    //   icon: 'icon-layers',
-    //   variant: 'danger',
-    // },
+
   ],
 };
